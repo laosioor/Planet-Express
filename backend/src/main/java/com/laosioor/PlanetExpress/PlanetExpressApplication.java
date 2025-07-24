89e3ba53dd67ceb2c958eb08c5e9d85de61cd0e8
Aloísio M. Lingo Filho <60991374+laosioor@users.noreply.github.com>
2025-07-23 22:08:52 -0300

    Working on backend structure

---
+++ backend/src/main/java/com/laosioor/PlanetExpress/PlanetExpressApplication.java
@@ -0,0 +1,13 @@
+package com.laosioor.PlanetExpress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanetExpressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanetExpressApplication.class, args);
+	}
+
+}
